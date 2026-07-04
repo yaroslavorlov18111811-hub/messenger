160451397ee01fb0e1e8d2d81c759a6a8c081557
yaroslavvorlov18111811-hub <yaroslavvorlov18111811@gmail.com>
2026-07-04 14:00:16 +0300

    Добавил is_moderator в админку

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'is_moderator', 'is_blocked', 'is_staff')
+    list_filter = ('is_moderator', 'is_blocked', 'is_staff')
+    search_fields = ('username', 'email')
+    fields = ('username', 'email', 'password', 'is_moderator', 'is_blocked', 'is_staff', 'is_active', 'is_superuser')
